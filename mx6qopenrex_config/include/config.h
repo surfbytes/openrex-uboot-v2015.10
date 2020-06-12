054426d58a26164e7463f65f57c1996f72ab5d36
surfbytes <surfbytes@gmail.com>
2020-06-11 17:06:25 -0700

    Minor cosmetic updates to match fedevel files for compares.

---
+++ mx6qopenrex_config/include/config.h
@@ -0,0 +1,12 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/fedevel/mx6openrex/mx6q_4x_mt41j256.cfg
+#define CONFIG_MX6Q	1
+#define CONFIG_DDR_SIZE	SZ_2G
+#define CONFIG_BOARD_TYPE_PREFIX	"imx6q-"
+#define CONFIG_BOARD_TYPE_POSTFIX	""
+#define CONFIG_BOARDDIR board/fedevel/mx6openrex
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6openrex.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6qopenrex_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,3 @@
+#define U_BOOT_DATE "Jun 11 2020"
+#define U_BOOT_TIME "15:21:04"
+#define U_BOOT_TZ "-0700"
